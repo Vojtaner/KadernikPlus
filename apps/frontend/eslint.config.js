ae0b85608065b45e939d71783e08b86589f571e9
Vojtaner <vojtech.laurin@gmail.com>
2025-06-19 19:56:25 +0200

    setup eslint and prettier

--- apps/frontend/eslint.config.js
+++ apps/frontend/eslint.config.js
@@ -1,8 +1,8 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
   {
