ff38c805824d4ffd59a09701af8b83fcdcb11de4
Vojtaner <vojtech.laurin@gmail.com>
2025-06-19 21:23:01 +0200

    added base setting

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,5 +1,33 @@
+import { createTheme, ThemeProvider } from '@mui/material'
+
+const theme = createTheme({
+  palette: {
+    success: {
+      main: '#42D029',
+    },
+    info: { main: '#2993D0' },
+    error: { main: '#D02929' },
+    warning: { main: '#D0B729' },
+    mode: 'light',
+    primary: {
+      main: '#D02964',
+      light: '#D0296420',
+      dark: '#D02964CC',
+    },
+    secondary: {
+      main: '#E33F5C',
+      light: '#E33F5C20',
+      dark: '#E33F5CCC',
+    },
+    text: {
+      primary: '#132847',
+      secondary: '#797979',
+    },
+  },
+})
+
 function App() {
-  return <></>
+  return <ThemeProvider theme={theme}></ThemeProvider>
 }
 
 export default App
---
+++ apps/frontend/src/components/layouts/MobileTabletLayout.tsx
@@ -0,0 +1,10 @@
+import { Stack } from "@mui/material"
+
+const MobileTabletLayout = (props:{children: React.ReactNode}) => {
+    return <Stack><Box></Box>
+
+    </Stack>
+}
+
+
+export default MobileTabletLayout
