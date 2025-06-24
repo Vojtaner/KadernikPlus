44e78e379e93f9159e26dd23dea58b3875dd0891
Vojtaner <vojtech.laurin@gmail.com>
2025-06-24 23:48:57 +0200

    enable docker

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   build: { outDir: '../../docs', emptyOutDir: true },
   plugins: [react()],
+  server: {
+    host: true,
+    port: 8080,
+  },
 })
