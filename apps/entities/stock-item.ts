cecc361c83598e686d1237bc1581d637ba135e31
Vojtaner <vojtech.laurin@gmail.com>
2025-07-14 18:43:25 +0200

    edit schema back

--- apps/entities/stock-item.ts
+++ apps/entities/stock-item.ts
@@ -1,27 +1,18 @@
-/**
- * Represents a StockItem entity in the domain.
- */
 export interface StockItem {
   id: string;
   name: string;
   unit: string;
   quantity: number;
   threshold: number;
-  isActive: boolean; // Changed from is_active to isActive
+  isActive: boolean;
   createdAt: Date;
   updatedAt: Date;
-  // Assuming StockAllowance is another entity you'll define
-  // stockAllowances: StockAllowance[]; // Add this if you need to represent the relation in the entity
 }
 
-/**
- * Interface for creating a new StockItem.
- * 'isActive' can be optional here, with a default value set in Prisma.
- */
 export interface StockItemCreateData {
   name: string;
   unit: string;
   quantity: number;
   threshold: number;
-  isActive?: boolean; // Changed from is_active to isActive
+  isActive?: boolean;
 }
