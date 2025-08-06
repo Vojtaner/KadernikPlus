426c072c3b89ca294c386c33080542c4ecb2565b
Vojtaner <vojtech.laurin@gmail.com>
2025-08-06 17:04:42 +0200

    change variables

--- apps/backend/src/application/services/auth0ManagementApi.ts
+++ apps/backend/src/application/services/auth0ManagementApi.ts
@@ -2,8 +2,8 @@ import { getEnvVar } from "../../utils/getEnvVar";
 import { ManagementClient } from "auth0";
 
 export const auth0ManagementApi = new ManagementClient({
-  clientId: getEnvVar("AUTH0_M2M_CLIENT_ID"),
   clientSecret: getEnvVar("AUTH0_M2M_CLIENT_SECRET"),
+  clientId: getEnvVar("AUTH0_M2M_CLIENT_ID"),
   domain: getEnvVar("AUTH0_M2M_DOMAIN"),
   audience: getEnvVar("AUTH0_M2M_AUDIENCE"),
 });
