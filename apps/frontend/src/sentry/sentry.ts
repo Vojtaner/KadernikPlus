04e4180a4db90440f2ee3911307b1a5d170fdf0e
Vojtaner <vojtech.laurin@gmail.com>
2025-09-09 22:02:44 +0200

    edit FE sentry

--- apps/frontend/src/sentry/sentry.ts
+++ apps/frontend/src/sentry/sentry.ts
@@ -4,6 +4,10 @@ const initializeSentry = () => {
   Sentry.init({
     dsn: 'https://6651c8da0140cfb0a365d8f77448a23a@o4509837349814272.ingest.de.sentry.io/4509837351649360',
     sendDefaultPii: true,
+    environment: process.env.IS_DEVELOPMENT === 'true' ? 'DEV' : 'PROD',
+    tracesSampleRate: process.env.IS_DEVELOPMENT === 'true' ? 1 : 0.4,
+    profilesSampleRate: process.env.IS_DEVELOPMENT === 'true' ? 1 : 0.4,
+    enableLogs: true,
   })
 }
 
