e15ad35d73ad4b934a2249aff03081ee0776f708
Vojtaner <vojtech.laurin@gmail.com>
2025-09-29 23:43:32 +0200

    edit

--- apps/frontend/env.sh
+++ apps/frontend/env.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env sh
-# Minimal env replacement script
 
 set -e
 
 APP_PREFIX="PREFIX_"
 ASSET_DIR="/usr/share/nginx/html"
 
-# Exit if asset directory does not exist
 [ -d "$ASSET_DIR" ] || exit 0
 
-# Iterate over environment variables starting with APP_PREFIX
 env | grep "^${APP_PREFIX}" | while IFS='=' read -r key value; do
     find "$ASSET_DIR" -type f \( -name "*.js" -o -name "*.html" \) -exec sed -i "s|${key}|${value}|g" {} +
 done
