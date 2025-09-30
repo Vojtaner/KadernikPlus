0e0ce4eb84dad9937c5b3106cffc3b8751355aeb
Vojtaner <139444684+Vojtaner@users.noreply.github.com>
2025-09-30 20:01:54 +0200

    Testbranch (#38)
    * test branch
    
    * edit cors
    
    * test PR branch
    
    * edit cors
    
    * edit cors

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -32,8 +32,8 @@ import {
   register,
 } from "./application/services/prometheus/prometheus";
 import invoiceRouter from "./routes/invoice-routes";
-import checkCors from "./utils/checkCors";
 import rateLimiter from "./utils/rateLimiter";
+import { checkCors } from "./utils/checkCors";
 
 dotenv.config();
 
@@ -53,6 +53,7 @@ const jwtCheck = auth({
 });
 
 app.use(metricsMiddleware);
+
 register.registerMetric(httpRequests);
 
 app.use(cors({ origin: checkCors, credentials: true }));
