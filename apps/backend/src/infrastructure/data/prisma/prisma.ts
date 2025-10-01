f0769b89fa23293b9b072871098a4e9bc3851f0f
Vojtaner <vojtech.laurin@gmail.com>
2025-10-01 09:54:55 +0200

    change

--- apps/backend/src/infrastructure/data/prisma/prisma.ts
+++ apps/backend/src/infrastructure/data/prisma/prisma.ts
@@ -1,10 +1,9 @@
 import { getEnvVar } from "../../../utils/getEnvVar";
 import { PrismaClient } from "@prisma/client";
 
-// const databaseUrl = `${getEnvVar("DATABASE_URL_BASE")}/${
-//   process.env.RAILWAY_GIT_BRANCH
-// }`;
-const databaseUrl = `${getEnvVar("DATABASE_URL_BASE")}/elina`;
+const databaseUrl = `${getEnvVar("DATABASE_URL_BASE")}/${
+  process.env.RAILWAY_GIT_BRANCH
+}`;
 
 const prisma = new PrismaClient({
   datasources: {
