a9d516933bb8079f7a2729a938844824553d0962
Vojtaner <vojtech.laurin@gmail.com>
2025-10-01 10:25:28 +0200

    test

--- apps/backend/entrypoint.sh
+++ apps/backend/entrypoint.sh
@@ -1,13 +1,16 @@
-#!/bin/sh
+#!/usr/bin/env sh
 set -e
 
-# Sestavíme runtime databázovou URL
+echo "--------------------------------------"
+echo "Starting entrypoint.sh"
+echo "RAILWAY_GIT_BRANCH: $RAILWAY_GIT_BRANCH"
+echo "DATABASE_URL_BASE: $DATABASE_URL_BASE"
+
+# Sestavíme finální DATABASE_URL
 export DATABASE_URL="${DATABASE_URL_BASE}/${RAILWAY_GIT_BRANCH}"
 
-echo "Using DATABASE_URL=$DATABASE_URL"
-
-# Spustíme migrace (volitelné, pokud chceš i při startu)
-# npx prisma migrate deploy
-
+echo "Resolved DATABASE_URL: $DATABASE_URL"
+echo "--------------------------------------"
+env | grep DATABASE_URL
 # Spustíme aplikaci
 exec npm run start
