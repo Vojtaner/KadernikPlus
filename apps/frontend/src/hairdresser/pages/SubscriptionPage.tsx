9e9505bc7cf778a96755123f1aae6cf574c08862
Vojtaner <vojtech.laurin@gmail.com>
2025-10-09 19:56:30 +0200

    edit subsription, transaltion,sign out button

--- apps/frontend/src/hairdresser/pages/SubscriptionPage.tsx
+++ apps/frontend/src/hairdresser/pages/SubscriptionPage.tsx
@@ -5,7 +5,7 @@ import { getSubscriptionText } from '../../entity'
 import { useAuth0 } from '@auth0/auth0-react'
 import AppTheme from '../../AppTheme'
 import LogoutIcon from '@mui/icons-material/Logout'
-import { useIntl } from 'react-intl'
+import { FormattedMessage, useIntl } from 'react-intl'
 import { getGreeting } from '../entity'
 
 export const SubscriptionPage = () => {
@@ -17,36 +17,78 @@ export const SubscriptionPage = () => {
   return (
     <Stack spacing={2} alignItems="center" justifyContent="center">
       <Typography variant="h4" fontWeight="bold" align="center">
-        Vítejte v aplikaci!
+        <FormattedMessage id="subscriptionPage.welcome" defaultMessage="Vítejte v aplikaci!" />
       </Typography>
       <Typography variant="h4" fontWeight="bold" color="primary">
-        Kadeřník+
+        <FormattedMessage id="subscriptionPage.headline" defaultMessage="Kadeřník+" />
       </Typography>
       <Typography variant="body1" color="text.secondary" textAlign="center" maxWidth={500} width="95vw">
         <strong style={{ color: AppTheme.palette.primary.main }}>
-          {user ? `Dobrý den, ${getGreeting(user)} ` : ''}
+          {user
+            ? `${intl.formatMessage({ id: 'subscriptionPage.greeting', defaultMessage: 'Dobrý den,' })} ${getGreeting(user)} `
+            : ''}
         </strong>
-        děkujeme za Váš zájem – naše služba funguje na bázi <strong>měsíčního předplatného</strong>, které se
-        <strong> automaticky obnovuje</strong>. Předplatné můžete kdykoliv zrušit v záložce <strong>MŮJ PROFIL</strong>.
-        Peníze za započatý měsíc jsou však nevratné.
+        {intl.formatMessage({
+          id: 'subscriptionPage.introduction.1',
+          defaultMessage: ' děkujeme za Váš zájem – naše služba funguje na bázi',
+        })}
+        <strong>
+          {intl.formatMessage({
+            id: 'subscriptionPage.introduction.2',
+            defaultMessage: ' měsíčního předplatného',
+          })}
+        </strong>
+        {intl.formatMessage({
+          id: 'subscriptionPage.introduction.3',
+          defaultMessage: ', které se',
+        })}
+        <strong>
+          {intl.formatMessage({
+            id: 'subscriptionPage.introduction.4',
+            defaultMessage: 'automaticky obnovuje',
+          })}
+        </strong>
+        {intl.formatMessage({
+          id: 'subscriptionPage.introduction.5',
+          defaultMessage: '. Předplatné můžete kdykoliv zrušit v záložce ',
+        })}
+        <strong>
+          {intl.formatMessage({
+            id: 'subscriptionPage.introduction.6',
+            defaultMessage: 'MŮJ PROFIL',
+          })}
+        </strong>
+        {intl.formatMessage({
+          id: 'subscriptionPage.introduction.7',
+          defaultMessage: '. Peníze za započatý měsíc jsou však nevratné.',
+        })}
       </Typography>
       {error && error.code === 410 && (
         <>
           <Typography color="info" align="center">
-            Mrzí nás to, Váš účet musel být před časem vymazán a nelze ho již znovu použít. Pro další postup nás
-            kontaktujte na <u style={{ fontWeight: 'bold' }}>podpora@kadernikplus.cz</u>
+            <FormattedMessage
+              id="subscriptionPage.deletedAccount"
+              defaultMessage=" Mrzí nás to, Váš účet musel být před časem vymazán a nelze ho již znovu použít. Pro další postup nás
+            kontaktujte na"
+            />
+            <u style={{ fontWeight: 'bold' }}>podpora@kadernikplus.cz</u>
           </Typography>
-          <Button variant="contained" onClick={() => logout()} startIcon={<LogoutIcon />}>
-            {intl.formatMessage({ id: 'logOut', defaultMessage: 'Odhlásit se' })}
-          </Button>
         </>
       )}
+      <Button variant="contained" onClick={() => logout()} startIcon={<LogoutIcon />}>
+        {intl.formatMessage({ id: 'logOut', defaultMessage: 'Odhlásit se' })}
+      </Button>
       {subscription && (
         <>
           <Typography variant="body1" color="info" textAlign="center" maxWidth={500}>
-            Vaše předplatné <strong>{getSubscriptionText(new Date(subscription.endDate), subscription.status)}</strong>
+            <FormattedMessage id="subscriptionPage.yourSubscription" defaultMessage="Vaše předplatné " />
+            <strong>{getSubscriptionText(new Date(subscription.endDate), subscription.status)}</strong>
           </Typography>
-          {subscription.status === 'ACTIVE' && <Button href={window.location.origin}>Přejít do aplikace</Button>}
+          {subscription.status === 'ACTIVE' && (
+            <Button href={window.location.origin}>
+              <FormattedMessage id="subscriptionPage.enterApp" defaultMessage="Přejít do aplikace" />
+            </Button>
+          )}
         </>
       )}
       {!(error && error.code === 410) && (
